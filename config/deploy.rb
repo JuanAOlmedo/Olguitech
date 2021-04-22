55609db16503abd42eb5e4ce4591d7564a38ac98
Juan Andres <olmedo.juannadres@tutanota.com>
2021-04-22 20:00:07 -0300

    Fixed some style bugs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.15.0"
+lock "~> 3.16.0"
 
 set :application, "olguitech"
 set :repo_url, "https://github.com/VarillasFriend/Olguitech.git"

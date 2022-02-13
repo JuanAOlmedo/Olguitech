400710b91d4498e28ac39f13bdc49dd0119b8451
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2022-02-13 20:05:39 -0300

    Fixed bug

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,9 +2,9 @@
 
 class MainController < ApplicationController
     def main
-        @articles = Article.published.order(created_at: :desc).last 4
-        @proyectos = Proyecto.published.order(created_at: :desc).last 4
-        @products = Product.published.order(created_at: :desc).last 4
+        @articles = Article.published.order(created_at: :desc).first 4
+        @proyectos = Proyecto.published.order(created_at: :desc).first 4
+        @products = Product.published.order(created_at: :desc).first 4
 
         @user = User.new
     end

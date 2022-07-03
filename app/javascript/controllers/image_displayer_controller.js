4676dd20b78342a87ec1f027a5ef30462a82e49c
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2022-07-03 18:26:51 -0300

    Polish image previews, add zoom while previewing

--- app/javascript/controllers/image_displayer_controller.js
+++ app/javascript/controllers/image_displayer_controller.js
@@ -4,14 +4,59 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = ['shower']
 
+    parent = null;
+    displayed = false;
+    zoomed= false;
+
     displayImage() {
-        this.showerTarget.style.display = 'flex';
-        this.showerTarget.children[0].src = event.target.src;
+        if (this.displayed) {
+            this.zoom(event);
+            return;
+        }
+
+        this.showerTarget.classList.add('show');
+        this.parent = event.target.parentElement;
+        this.parent.style.height = event.target.height + 'px';
+        this.showerTarget.append(event.target);
         document.documentElement.style.overflowY = 'hidden';
+        
+        this.displayed = true;
     }
 
     stopDisplayImage() {
-        this.showerTarget.style.display = 'none';
+        const img = this.showerTarget.children[0];
+        if (event.target == img) { return; }
+
+        this.showerTarget.classList.remove('show');
+        this.zoomOut(img);
+        this.parent.prepend(img);
+        this.parent.style.height = '';
         document.documentElement.style.overflowY = 'auto';
+
+        this.displayed = false;
+    }
+
+    zoom(e) {
+        if (this.zoomed) { 
+            this.zoomOut(e.target);
+            return;
+        }
+
+        const bounds = e.target.getBoundingClientRect(),
+              transformX = bounds.x + bounds.width * 0.5 - e.clientX,
+              transformY = bounds.y + bounds.height * 0.5 - e.clientY;
+
+        e.target.style.transform = `translateX(${transformX}px)
+                                    translateY(${transformY}px)
+                                    scale(2)`
+        e.target.style.cursor = 'zoom-out';
+        
+        this.zoomed = true
+    }
+
+    zoomOut(img) {
+        img.style.transform = '';
+        img.style.cursor = '';
+        this.zoomed = false;
     }
 }

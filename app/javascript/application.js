8834c6f552632ddb4149536a5760ea9ad6b9078b
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2022-11-28 10:59:33 -0300

    Fix some issues for older browsers

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
+import "controllers";
 import "trix-editor-overrides";
+import "webp";
 import "@hotwired/turbo-rails";
 import "@hotwired/stimulus";
 import "@hotwired/stimulus-loading";
-import "controllers";
 import "@rails/actiontext";
-import "webp";

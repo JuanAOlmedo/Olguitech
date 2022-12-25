4c63b5d82d5149fefa859d9d40f9cdc274b3ec9d
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2022-12-25 12:29:13 -0300

    Update ruby

--- config/application.rb
+++ config/application.rb
@@ -31,11 +31,9 @@ module Olguitech
 
         config.before_configuration do
             env_file = File.join(Rails.root, 'config', 'local_env.yml')
-            YAML
-                .load(File.open(env_file))
-                .each { |key, value| ENV[key.to_s] = value } if File.exists?(
-                env_file,
-            )
+
+            YAML.load(File.open(env_file))
+                .each { |key, value| ENV[key.to_s] = value } if File.exist?(env_file)
         end
     end
 end

ba1937b086feae1cffb7f883d82431a278a619d8
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2023-01-31 11:47:35 -0300

    Make SQL queries more efficient, improve style

--- app/models/concerns/getters.rb
+++ app/models/concerns/getters.rb
@@ -37,46 +37,9 @@ module Getters
         base.extend(ClassMethods)
     end
 
-    # Used for the class method :ordered, which is used in articles, products and
-    # projects index
     module ClassMethods
-        def ordered(order_by, asc_desc)
-            order_by = test_order_by order_by
-            asc_desc = %w[asc desc].include?(asc_desc) ? asc_desc : :desc
-
-            return_ordered(order_by, asc_desc)
-        end
-
-        private
-
-        # Test that order_by is in a certain list, else change order_by to 'categories'
-        # Return localized title or order_by accordingly
-        def test_order_by(order_by)
-            order_by =
-                if %w[created_at updated_at title categories uncategorized].include? order_by
-                    order_by
-                else
-                    'categories'
-                end
-
-            order_by == 'title' && I18n.locale == :en ? 'title2' : order_by
-        end
-
-        # Return an array containing all categories associated to the model,
-        # or just the instances order according to order_by
-        def return_ordered(order_by, asc_desc)
-            case order_by
-            when 'categories'
-                name = model_name.plural.to_sym
-                categories =
-                    Category.includes(name).where.associated(name).where(name => { status: 0 }).uniq
-            when 'uncategorized'
-                ordered = published.where.missing(:categories).order created_at: asc_desc
-            else
-                ordered = published.order order_by => asc_desc
-            end
-
-            [categories, ordered]
+        def fields_for_cards
+            [:id, :slug, :title, :title2, :description, :description2]
         end
     end
 end

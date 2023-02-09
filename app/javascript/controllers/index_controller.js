d4cc28c69c104e3d4ed3ff9f05601604184544dc
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2023-02-08 22:00:48 -0300

    Change style of super_categories in index

--- app/javascript/controllers/index_controller.js
+++ app/javascript/controllers/index_controller.js
@@ -3,8 +3,10 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="index"
 // Controls the articles, products and projects index.
 export default class extends Controller {
-    scrollToAnchor(event) {
-        event.preventDefault();
-        location.hash = event.target.attributes.href.value;
+    expandMenu() {
+        const content = event.target.nextElementSibling;
+
+        event.target.classList.toggle("active");
+        content.classList.toggle("active")
     }
 }

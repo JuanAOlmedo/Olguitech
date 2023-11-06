10ac403700ced76c64812be6fdb0046837de0426
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2023-11-06 17:01:45 -0300

    Update capistrano info

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.0"
+lock "~> 3.18.0"
 
 set :application, "olguitech"
 set :repo_url, "https://github.com/VarillasFriend/Olguitech.git"

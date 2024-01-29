45e8486dcf27298b4be286f686c8994876fe68d3
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2024-01-28 22:07:29 -0300

    Update bundle, add comment

--- app/controllers/contactos_controller.rb
+++ app/controllers/contactos_controller.rb
@@ -12,10 +12,10 @@ class ContactosController < ApplicationController
         @user.locale = I18n.locale
         @contacto = @user.contactos.new(contacto_params)
 
+        # Verify users with a captcha
         unless verify_hcaptcha(model: @contacto)
             render(:index, status: :unprocessable_entity) and return
         end
-
 
         if @user.name && @user.phone && @user.company
             process_contact

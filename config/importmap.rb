1d8811b228c3808cfa31cb820721d684526d3c45
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2024-06-21 15:34:56 -0300

    Update bundle, bundler and fix trix error

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,12 +1,12 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "trix-editor-overrides"
-pin "webp"
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin 'application', preload: true
+pin 'trix-editor-overrides'
+pin 'webp'
+pin_all_from 'app/javascript/controllers', under: 'controllers'
 
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.js"
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin 'trix', to: 'https://ga.jspm.io/npm:trix@2.1.1/dist/trix.esm.min.js', preload: false
+pin '@rails/actiontext', to: '@rails--actiontext.js', preload: false # @7.1.3

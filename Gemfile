ecce5656b3a7c390df8d185b92e5e340ace0cab7
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2024-07-23 19:08:09 -0300

    Update bundle, update rails to 7.1

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.3.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '7.0.8.4'
+gem 'rails', '7.1.3.4'
 
 # Use pg as the database for Active Record in production
 gem 'pg'
--- config/initializers/action_text.rb
+++ config/initializers/action_text.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ActionText::ContentHelper.allowed_attributes.add 'style'
-ActionText::ContentHelper.allowed_attributes.add 'data-action'
-ActionText::ContentHelper.allowed_attributes.add 'data-nowebpsrc'
-ActionText::ContentHelper.allowed_attributes.add 'loading'
+# Whitelist attributes to use them in views/action_text.
+# NOTE: According to the documentation, ActionText::ContentHelper.sanitizer_allowed_attributes
+# whould be used to access the default values, but that doesn't work.
+ActionText::ContentHelper.allowed_attributes = ActionText::ContentHelper.sanitizer.class.allowed_attributes + ['style', 'data-action', 'data-nowebpsrc', 'loading']

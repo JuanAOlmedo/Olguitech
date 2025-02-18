046e3c31234b23490776e67cd2f0371d6fdd955d
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2025-02-18 15:27:27 -0300

    Refactor MessagesController

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,21 @@
+# frozen_string_literal: true
+
 class MessagesController < ApplicationController
-    # Find or create a new user based on the email and create a new contact
+    # Find or create a new user based on the email, create a new message
     # for that user and update their locale
     def create
         @user = User.find_by(user_params) || User.new(user_params)
-        @user.locale = I18n.locale
         @message = @user.messages.new message_params
+        @user.locale = I18n.locale
 
-        # Verify users with a captcha
-        if Rails.env == 'production' && !verify_hcaptcha(model: @message)
-            render(:index, status: :unprocessable_entity) and return
-        end
+        verify_captcha
 
-        if @user.name && @user.phone && @user.company
-            process_contact
+        if @message.save && @user.save
+            @message.send_mail
+
+            redirect_user
         else
-            process_contact_for_incomplete_user
+            render 'main/contacto', status: :unprocessable_entity
         end
     end
 
@@ -28,28 +29,20 @@ class MessagesController < ApplicationController
         params.require(:user).permit :email
     end
 
-    # If the contact saves successfully, send the user and the admin an email
-    def process_contact
-        if @message.save
-            @message.send_mail
-
-            redirect_to root_path, notice: I18n.t('contact.sent')
-        else
-            render :index, status: :unprocessable_entity
+    def verify_captcha
+        if !verify_hcaptcha(model: Message.new) && Rails.env == 'production'
+            render 'main/contacto', status: :unprocessable_entity
         end
     end
 
-    # If the user doesn't have all fields (name, phone company) filled, take them to
-    # the edit_user path to fill them up. Add a contact parameter so that the views
-    # can show information according to the context
-    def process_contact_for_incomplete_user
-        if @user.save && @message.save
-            @message.send_mail
-
+    # Redirect the user to the appropriate page based on their status
+    # Redirects to root_path if all fields are filled, otherwise redirects to edit_user_path
+    def redirect_user
+        if @user.name && @user.phone && @user.company
+            redirect_to root_path, notice: I18n.t('contact.sent')
+        else
             redirect_to edit_user_path(@user, edit_token: @user.edit_token, contact: true),
                         notice: I18n.t('contact.first_time')
-        else
-            render :index, status: :unprocessable_entity
         end
     end
 end

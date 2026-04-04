b2cc9f1c50148a317432ca2269e7e96180c90e36
Juan Andrés Olmedo <olmedo.juanandres5@gmail.com>
2026-04-04 20:24:42 -0300

    Actualizar bundle, borrar archivo innecesario

--- config/initializers/recaptcha.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Recaptcha.configure do |config|
-    config.site_key = Rails.application.credentials.CAPTCHA_SITE_KEY
-    config.secret_key = Rails.application.credentials.CAPTCHA_SECRET_KEY
-end

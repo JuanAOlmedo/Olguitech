1b58a1fe4aa2842312ae2edee46ecbbf8747d4a2
Juan Andrés Olmedo <olmedo.juanandres5@gmail.com>
2026-06-07 18:48:26 -0300

    Actualizar bundle y rails

---
+++ app/controllers/concerns/throttle.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# Cuenta la cantidad de contactos y decide si es necesario bloquear una request.
+# Requiere Rails.cache, por lo que tiene que estar configurado
+module Throttle
+    MAX_CONTACTS = 10
+    TIME_WINDOW = 1.day
+
+    def contact_count
+        Rails.cache.write key, 0, raw: true, expires_in: TIME_WINDOW, unless_exist: true
+        Rails.cache.read(key, raw: true).to_i
+    end
+
+    def increment_contact_count
+        Rails.cache.write key, 0, raw: true, expires_in: TIME_WINDOW, unless_exist: true
+        Rails.cache.increment key
+    end
+
+    def block_contact?
+        contact_count >= MAX_CONTACTS
+    end
+
+    private
+
+    def key
+        "contactos_#{Time.now.to_i / TIME_WINDOW.to_i}"
+    end
+end
